af3b1ce935043dfc5bc1b2f199fd16cfc5525851
Marco.Andrey <marco.andrey1208@gmail.com>
2026-08-19 22:37:16 -0500

    Actualizacion 19 AGO 2026

---
+++ js/05.js
@@ -0,0 +1,19 @@
+// Numeros
+
+const nombre = "Marco"; //String
+const numero1 = 100;
+const numero2 = 200; // numero
+
+// Cuando ves en consola y esta en negro es un string si esta en azul es numero asi es como se puede diferenciar
+
+console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+
+// Operaciones
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero1 * numero2);
+console.log(numero2 / numero1);
+console.log(numero1 % numero2);
---
+++ js/06.js
@@ -0,0 +1,16 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.6); //Redondea la cifra
+resultado = Math.ceil(2.1); //Redondea siempre hacia arriba
+resultado = Math.floor(2.1); //Redondea siempre hacia abajo
+resultado = Math.sqrt(777); //Raiz cuadrada
+resultado = Math.abs(-200); //De negativo lo pasa a positivo
+resultado = Math.min(3, 5, 1, 8, 2, 10); //Nos muestra el minimo numero de un listado
+resultado = Math.max(3, 5, 1, 8, 2, 10); //Nos muestra el Maximo numero de un listado
+resultado = Math.random(); //Nos muestra un numero random
+resultado = Math.floor(Math.random() * 30); //Nos muestra un numero aleatorio
+
+console.log(resultado);
